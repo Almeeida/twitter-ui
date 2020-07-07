3f84fb92b24e3f874026b084ce8be614ca4868e8
Almeeida <almeidx@pm.me>
2020-07-07 23:31:45 +0100

    Updates

---
+++ precache-manifest.ec0d140839ea476ee1566a9be0e8af38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2bccef91d506e8dc45c8c34f4ec8b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5bde7f51d705cb8d691",
+    "url": "/static/js/2.7e738b44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7e738b44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ab4bea862341c32ef90",
+    "url": "/static/js/main.6fd20411.chunk.js"
+  },
+  {
+    "revision": "c3dff0d2d5bb15a1d5c9",
+    "url": "/static/js/runtime-main.27865748.js"
+  }
+]);
